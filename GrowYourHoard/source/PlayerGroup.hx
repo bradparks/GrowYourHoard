05e2016ee161e3455e68daddefc7d36e4debbbb9
John Doughty <jrdoughty13@gmail.com>
2015-12-12 20:07:18 -0600

    commiting player group for pouchin

--- GrowYourHoard/source/PlayerGroup.hx
+++ GrowYourHoard/source/PlayerGroup.hx
@@ -3,22 +3,25 @@ package;
 import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxG;
+import flixel.group.FlxGroup;
 
 /**
  * ...
  * @author John Doughty
  */
-class Player extends FlxSprite
+class PlayerGroup extends FlxGroup
 {
+	var goblin:FlxSprite;
 	public function new(X:Float=0, Y:Float=0)
 	{
-		super(X, Y);
-
-		allowCollisions = FlxObject.CEILING;
-		immovable = true;
-		loadGraphic("assets/images/shield.png",true, 8, 16);
-		animation.add("main",[0,1],4,true);
-		setGraphicSize(20, 40);
+		super();
+		goblin = new FlxSprite(X, Y);
+		goblin.allowCollisions = FlxObject.CEILING;
+		goblin.immovable = true;
+		goblin.loadGraphic("assets/images/shield.png",true, 8, 16);
+		goblin.animation.add("main",[0,1],4,true);
+		goblin.setGraphicSize(20, 40);
+		add(goblin);
 	}
 
 	override public function update():Void
@@ -27,23 +30,23 @@ class Player extends FlxSprite
 
 		if (FlxG.keys.anyJustPressed(["A","D","LEFT","RIGHT"]) || FlxG.mouse.justPressed || FlxG.mouse.justPressedRight)
 		{
-			animation.play("main");
+			goblin.animation.play("main");
 		}
 		else if (!(FlxG.keys.anyPressed(["A", "D"]) || FlxG.mouse.pressed || FlxG.mouse.pressedRight))
 		{
-			animation.frameIndex = 0;
-			animation.pause();
+			goblin.animation.frameIndex = 0;
+			goblin.animation.pause();
 		}
 
 		if (FlxG.keys.pressed.A || FlxG.keys.pressed.LEFT || FlxG.mouse.pressed)
 		{
-			flipX = true;
-			x -= 2;
+			goblin.flipX = true;
+			goblin.x -= 2;
 		}
 		if (FlxG.keys.pressed.D || FlxG.keys.pressed.RIGHT || FlxG.mouse.pressedRight)
 		{
-			flipX = false;
-			x += 2;
+			goblin.flipX = false;
+			goblin.x += 2;
 		}
 	}
 }
