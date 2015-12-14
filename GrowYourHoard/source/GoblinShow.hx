61828d20a08f0e08d43e82457dd8083238c3e7c1
John Doughty <jrdoughty13@gmail.com>
2015-12-13 19:23:54 -0600

    adding soldier and tweaking show hoard

---
+++ GrowYourHoard/source/GoblinShow.hx
@@ -0,0 +1,19 @@
+package;
+
+/**
+ * ...
+ * @author John Doughty
+ */
+class GoblinShow extends Goblin
+{
+
+	override function getScore() 
+	{
+		return 0;
+	}
+	
+	override function getTargetY() 
+	{
+		return 125;
+	}
+}
